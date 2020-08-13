2d99e8426ac27d4744c9bb428129f3a873a52639
granwyntan <granwyn_tan@s2019.ssts.edu.sg>
2020-08-13 10:12:42 +0800

    Initial Commit

---
+++ Viraze/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Viraze
+//
+//  Created by Granwyn Tan on 13/8/20.
+//  Copyright © 2020 Granwyn Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
